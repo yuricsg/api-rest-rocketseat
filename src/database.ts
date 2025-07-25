4796c4777a114209ca73d8b2f8145b324f480acb
yuricsg <yuriicardo004@gmail.com>
2025-07-25 08:17:22 -0300

    criando conexão com bd usando knex

---
+++ src/database.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import knex from 'knex'
+
+export const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './tmp/app.db',
+  },
+})
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { db } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await db('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
